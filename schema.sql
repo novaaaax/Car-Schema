e8e51d0e73443aff59ac8e998bfb8790b8731b6e
Edwin <edwingenova@gmail.com>
2020-04-21 10:29:24 -0400

    added schema and seeds

---
+++ schema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS cars_db;
+
+CREATE DATABASE cars_db;
+
+--created our cars database where our make, models, and pastsearches tables live
---
+++ seeds.sql
@@ -0,0 +1,51 @@
+INSERT INTO Makes(name)
+VALUES("Acura"),("Alfa Romeo"),("Aston Martin"),
+("BMW"),("Buick"),("Cadillac"),
+("Chevrolet"),("Chrysler"),("Dodge"),("Ferrari"),
+("Fiat"),("Ford"),("GMC"),("Honda"),("Infiniti"),("Jaguar"),("Jeep"),
+("Lamborghini"),("Land Rover"),("Lexus"),("Maserati"),
+("McLaren"),("Mitsubishi"),("Porsche"),("RAM"),
+("Rolls Royce"),("Subaru"),("Tesla"),("Toyota"),
+("Volkswagen"),("Volvo");
+
+INSERT INTO Models(name, makeId)
+VALUES("ILX", 1),("RDX", 1),("RLX", 1),("MDX", 1),
+("NSX", 1),("TLX", 1),
+("Spider", 2),("Stelvio", 2),
+("DB11", 3),("Vantage", 3),("Flying Spur", 3),
+("230i", 4),("328d", 4),("330e", 4),("330i", 4),
+("340i", 4),("430i", 4),("440i", 4),("M240i", 4),
+("M3", 4),("M4", 4),("M5", 4),
+("LaCrosse", 5),("Regal", 5),
+("CTS", 6),("ATS", 6),("XTS", 6),
+("Camaro", 7),("Corvette", 7),("Cruze",7),
+("Impala", 7),("Malibu", 7),("Silverado", 7),
+("300", 8),("Pacifica", 8),
+("Challenger", 9),("Durango", 9),
+("Charger", 9),
+("Portofino", 10),("488 GTB", 10), ("812 Superfast", 10),
+("500c", 11),("500x", 11),
+("Fiesta", 12),("Focus", 12),("Fusion", 12),("Mustang", 12),
+("Acadia", 13),
+("Accord", 14),("Civic", 14),("CR-V", 14),("HR-V", 14),
+("Q50", 15),("Q60", 15),("Q70", 15),
+("XE", 16),("XF", 16),("F-TYPE", 16), 
+("Wrangler", 17),("Compass", 17),("Grand Cherokee", 17),
+("Aventador", 18),("Huracan", 18),
+("Range Rover", 19),
+("RC", 20),("LC", 20),
+("GranTurismo", 21),("Levante", 21),("Ghibli", 21),
+("720S", 22),("570S", 22),
+("Eclipse", 23),("Mirage", 23),("Outlander", 23),
+("911", 24),("Cayenne", 24),("Panamera",24),
+("1500", 25),("2500", 25),("3500", 25),
+("Ghost", 26),("Phantom", 26),("Wraith", 26),
+("BRZ", 27),("WRX", 27),("Impreza", 27),("Outback", 27),
+("Model 3", 28),("Model S", 28),("Model X", 28),
+("Camry", 29),("Corolla", 29),("86", 29),
+("Golf", 30),("Passat", 30),("Beetle", 30),
+("S60", 31),("S90", 31),("XC90", 31)
+
+--this is all the data in our tables, this information is used to populate the drop down menu
+--we used the foreign key method to match all models with their proper make
+--once its selected and the user clicks enter, it is then pushed to the url for the api we used
